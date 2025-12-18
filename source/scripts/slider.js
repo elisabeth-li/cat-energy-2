e45109baef3e2b5acfc59202a0d0f3e5456b6fd6
elisabeth_li <kitsune88.li@gmail.com>
2025-12-19 00:08:25 +0300

    Правки

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,6 +1,9 @@
 const slider = document.querySelector('.example__slider');
 const range = document.querySelector('.slider__range-js');
+<<<<<<< HEAD
 
+=======
+>>>>>>> bba61ac93569807b1aee3ea99045fc99fc2fa7aa
 range.addEventListener('input', () => {
   slider.style.setProperty('--value', `${range.value }%`);
 });
